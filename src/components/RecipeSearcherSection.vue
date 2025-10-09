1a0f7bef7468aeb4a7d13b1e7a27ace8935de8ff
Arthaud <arthaudp33@gmail.com>
2025-10-09 17:04:07 +0200

    :bug: Fix small UX issues

--- src/components/RecipeSearcherSection.vue
+++ src/components/RecipeSearcherSection.vue
@@ -1,41 +1,47 @@
 <template>
   <section class="flex flex-col gap-2">
-    <header class="sticky top-0 z-10 bg-white py-2 space-y-2">
+    <header class="sticky top-0 z-10 bg-white py-2">
       <form role="search" autocomplete="off">
         <label class="flex gap-2">
           <input
             v-model="searcher.query.value"
-            class="w-full py-2 px-3 bg-gray-100 rounded-lg"
+            class="w-full py-3 px-3 bg-gray-100 rounded-lg"
             type="text"
             placeholder="Rechercher un ingrédient, appareil, ustensiles ou une recette"
           />
         </label>
       </form>
 
-      <div class="flex gap-2">
+      <div class="mt-2 flex gap-2">
         <TagSelector
+          class="flex-1"
           :options="searcher.availableIngredients.value"
           v-model="searcher.selectedIngredients.value"
+          :disabled="!searcher.availableIngredients.value.length"
           placeholder="Rechercher un ingrédient"
           type="ingredient"
         />
 
         <TagSelector
+          class="flex-1"
           :options="searcher.availableAppliances.value"
           v-model="searcher.selectedAppliances.value"
+          :disabled="!searcher.availableAppliances.value.length"
           placeholder="Rechercher un appareil"
           type="appliance"
         />
 
         <TagSelector
+          class="flex-1"
           :options="searcher.availableUstensils.value"
           v-model="searcher.selectedUstensils.value"
+          :disabled="!searcher.availableUstensils.value.length"
           placeholder="Rechercher un ustensile"
           type="ustensil"
         />
       </div>
 
-      <section class="flex gap-2">
+      <section class="mt-4 flex gap-2">
         <SearchItem
           v-for="ingredient in searcher.selectedIngredients.value"
           :key="ingredient"
--- src/components/TagSelector.vue
+++ src/components/TagSelector.vue
@@ -1,61 +1,64 @@
 <template>
-  <div class="w-72">
-    <Combobox v-model="selected" multiple>
-      <div class="relative mt-1">
-        <div
-          class="relative w-full cursor-default overflow-hidden rounded-lg text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300"
+  <Combobox v-model="selected" multiple :disabled="disabled" v-slot="{ open }">
+    <div class="relative">
+      <div
+        class="relative w-full cursor-default overflow-hidden rounded-lg text-left focus:outline-none flex"
+        :class="[clx.bg, clx.text]"
+      >
+        <ComboboxInput
+          class="w-full border-none py-3 pl-3 text-base leading-5 focus:ring-0 focus:outline-none"
+          :class="[clx.bg, clx.text, disabled && 'opacity-50']"
+          :placeholder="placeholder"
+          :disabled="disabled"
+          @change="query = $event.target.value"
+        />
+        <ComboboxButton
+          class="flex items-center px-2"
+          :class="[disabled && 'opacity-50', open || 'fluid-link']"
+        >
+          <ChevronUpDownIcon class="h-5 w-5" :class="[clx.text]" aria-hidden="true" />
+        </ComboboxButton>
+      </div>
+      <TransitionRoot
+        leave="transition ease-in duration-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+        @after-leave="query = ''"
+      >
+        <ComboboxOptions
+          v-if="!disabled"
+          class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none"
           :class="[clx.bg, clx.text]"
         >
-          <ComboboxInput
-            class="w-full border-none py-2 pl-3 pr-10 text-base leading-5 focus:ring-0 focus:outline-none"
-            :class="[clx.bg, clx.text]"
-            :placeholder="placeholder"
-            @change="query = $event.target.value"
-          />
-          <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronUpDownIcon class="h-5 w-5" :class="[clx.text]" aria-hidden="true" />
-          </ComboboxButton>
-        </div>
-        <TransitionRoot
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-          @after-leave="query = ''"
-        >
-          <ComboboxOptions
-            class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none"
-            :class="[clx.bg, clx.text]"
+          <div
+            v-if="filteredOptions.length === 0 && query !== ''"
+            class="relative cursor-default select-none px-4 py-2"
           >
-            <div
-              v-if="filteredOptions.length === 0 && query !== ''"
-              class="relative cursor-default select-none px-4 py-2"
-            >
-              Aucun résultat.
-            </div>
+            Aucun résultat.
+          </div>
 
-            <ComboboxOption
-              v-for="option in filteredOptions"
-              as="template"
-              :key="option"
-              :value="option"
-              v-slot="{ active }"
+          <ComboboxOption
+            v-for="option in filteredOptions"
+            as="template"
+            :key="option"
+            :value="option"
+            v-slot="{ active }"
+          >
+            <li
+              class="relative cursor-default select-none py-2 px-4"
+              :class="{
+                'bg-neutral-900/5': active,
+              }"
             >
-              <li
-                class="relative cursor-default select-none py-2 px-4"
-                :class="{
-                  'bg-neutral-900/5': active,
-                }"
-              >
-                <span class="block truncate">
-                  {{ option }}
-                </span>
-              </li>
-            </ComboboxOption>
-          </ComboboxOptions>
-        </TransitionRoot>
-      </div>
-    </Combobox>
-  </div>
+              <span class="block truncate">
+                {{ option }}
+              </span>
+            </li>
+          </ComboboxOption>
+        </ComboboxOptions>
+      </TransitionRoot>
+    </div>
+  </Combobox>
 </template>
 
 <script setup lang="ts">
@@ -74,6 +77,7 @@ const props = defineProps<{
   options: Array<string>
   placeholder?: string
   type: 'ingredient' | 'ustensil' | 'appliance'
+  disabled?: boolean
 }>()
 
 const selected = defineModel<Array<string>>({
